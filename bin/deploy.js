0803042bf11119d5c583f2cfd8d5d78ff53e2acf
Aaron Alaniz <aalaniz@twilio.com>
2021-07-28 09:34:39 -0500

    Update deploy.js (#4)

--- bin/deploy.js
+++ bin/deploy.js
@@ -3,44 +3,40 @@
 const fse = require('fs-extra');
 const appAssetsDir = 'assets' 
 const appBuildDir = 'apps/web/build' 
-const exec = require('child_process').exec;
+const promisify = require('util').promisify;
+const exec = promisify(require('child_process').exec);
 
-new Promise((resolve, reject) => {
-  exec('npm --prefix composer run build', error => error ? reject(error) : resolve())
-}).then(() => {
-  new Promise((resolve, reject) => {
-    exec('npm --prefix apps/web run build', error => error ? reject(error) : resolve())
-  })
-}).then(() => {
-  /**
-   * Copy the build outputs to the assets directory for twilio-run to deploy
-   */
-  new Promise((_) => {
+(async () => {
+  try {
+    /**
+     * Build the app projects
+     */
+    await exec('npm --prefix composer run build');
+    await exec('npm --prefix apps/web run build');
+
+    /**
+     * Copy the build outputs to the assets directory for twilio-run to deploy
+    */
     if (fse.existsSync(appAssetsDir)) {
-      fse.removeSync(appAssetsDir)
+      fse.removeSync(appAssetsDir);
     }
-    fse.mkdirSync(appAssetsDir)
-    fse.copySync(appBuildDir, appAssetsDir)
-  })
-}).then(() => {
-  new Promise((resolve, reject) => {
-    exec('twilio-run deploy --override-existing-project', (error, stdout, stderr) => {
-      if (error) {
-        reject(error)
-      } else {
-        const twilioRunOutputRegex = RegExp('domain\\s+(?<domain>.*)', 'm')
-        const match = stdout.match(twilioRunOutputRegex)
-        if (match.groups.domain) {
-          const appUrl = `https://${match.groups.domain.trim()}/index.html`
-          console.log(`App: ${appUrl}`)
-          resolve()
-        } else {
-          reject(Error('Failed to extract app url'))
-        }
-      }
-    })
-  })
-}).catch(error => {
-  console.error(error);
-  process.exit(1);
-});
+    fse.mkdirSync(appAssetsDir);
+    fse.copySync(appBuildDir, appAssetsDir);
+
+    /**
+     * Deploy the builds and extract user output from the twilio-run command
+     */
+    const { stdout } = await exec('twilio-run deploy --override-existing-project');
+    const twilioRunOutputRegex = RegExp('domain\\s+(?<domain>.*)', 'm');
+    const match = stdout.match(twilioRunOutputRegex);
+    if (match.groups?.domain) {
+      const appUrl = `https://${match.groups.domain.trim()}/index.html`;
+      console.log(`App: ${appUrl}`);
+    } else {
+      throw Error('Failed to extract app url');
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})()
