64ac9ad3441ff787cbd5cb10e4f2661daa2673eb
Tim Rozum <trozum@twilio.com>
2021-08-26 09:59:37 -0600

    VIDEO-6584: Add iOS app (#5)
    * Add new project and change file header comment
    
    * Make CI run iOS tests
    
    * Update .gitignore
    
    * Update fastlane config
    
    * Fix .gitignore
    
    * More gitignore
    
    * Remove apache license
    
    * Remove main branch filter

---
+++ apps/ios/LiveVideo/LiveVideo/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright (C) 2021 Twilio, Inc.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
