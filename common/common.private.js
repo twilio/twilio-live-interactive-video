f0ddc956ec67c378b680332625ff2a2546d427f2
Tim Mendoza <tmendoza@twilio.com>
2021-09-01 19:41:24 -0600

    Revert "Add support for dev and stage"
    This reverts commit 2247af984d8781786b71a137a64bd16cfc3e5e15.

--- common/common.private.js
+++ common/common.private.js
@@ -6,16 +6,12 @@ module.exports = (context, event, callback) => {
   let response = new Twilio.Response();
   response.appendHeader('Content-Type', 'application/json');
 
-  const {region} = context.getTwilioClient();
-
   const axiosClient = axios.create({
     headers: {
       Authorization: 'Basic ' + Buffer.from(`${context.ACCOUNT_SID}:${context.AUTH_TOKEN}`, 'utf8').toString('base64'),
       'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
     },
-    baseURL: (region === 'dev' || region === 'stage')
-      ? `https://media.${region}.twilio.com/v1`
-      : 'https://media.twilio.com/v1',
+    baseURL: 'https://media.twilio.com/v1/',
   });
 
   async function getPlaybackGrant(livePlayerStreamerSid) {
