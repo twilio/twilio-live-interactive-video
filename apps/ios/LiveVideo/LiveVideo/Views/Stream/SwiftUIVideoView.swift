31f01832971cb3ddfc283e9f1bed036a37185e62
Tim Rozum <trozum@twilio.com>
2021-09-29 15:46:40 -0600

    Fix video view memory leak

--- apps/ios/LiveVideo/LiveVideo/Views/Stream/SwiftUIVideoView.swift
+++ apps/ios/LiveVideo/LiveVideo/Views/Stream/SwiftUIVideoView.swift
@@ -5,26 +5,45 @@
 import SwiftUI
 import TwilioVideo
 
+/// A SwiftUI video view that is automatically removed from the video track when the view is no longer in use.
 struct SwiftUIVideoView: UIViewRepresentable {
     @Binding var videoTrack: VideoTrack?
     @Binding var shouldMirror: Bool
 
-    func makeUIView(context: Context) -> VideoView {
-        let videoView = VideoView()
+    func makeUIView(context: Context) -> VideoTrackStoringVideoView {
+        let videoView = VideoTrackStoringVideoView()
         videoView.contentMode = .scaleAspectFill
         return videoView
     }
 
-    func updateUIView(_ uiView: VideoView, context: Context) {
-        if let videoTrack = videoTrack {
-            if !videoTrack.isRendered(by: uiView) {
-                videoTrack.addRenderer(uiView)
-            }
-        } else {
-            videoTrack?.removeRenderer(uiView)
-        }
-        
+    func updateUIView(_ uiView: VideoTrackStoringVideoView, context: Context) {
+        uiView.videoTrack = videoTrack
         uiView.shouldMirror = shouldMirror
+    }
+    
+    static func dismantleUIView(_ uiView: VideoTrackStoringVideoView, coordinator: ()) {
+        uiView.videoTrack?.removeRenderer(uiView)
+    }
+}
+
+/// A `VideoView` that stores a reference to the `VideoTrack` it renders.
+///
+/// This makes it easy to update view state when the `VideoTrack` changes. Just set the `VideoTrack` and the
+/// view will handle`addRenderer` and `removeRenderer` automatically.
+///
+/// It also provides a `VideoTrack` reference to `SwiftUIVideoView` so that `SwiftUIVideoView` can
+/// remove the `VideoView` from the `VideoTrack` when `dismantleUIView` is called.`
+class VideoTrackStoringVideoView: VideoView {
+    var videoTrack: VideoTrack? {
+        didSet {
+            if let videoTrack = videoTrack {
+                if !videoTrack.isRendered(by: self) {
+                    videoTrack.addRenderer(self)
+                }
+            } else {
+                oldValue?.removeRenderer(self)
+            }
+        }
     }
 }
 
