fe776bc61bb0b17ec0062da6c9d3be032120f1df
Tim Rozum <trozum@twilio.com>
2021-10-15 12:51:00 -0600

    Remove old code

--- apps/ios/LiveVideo/LiveVideo/Twilio/Sync/ViewerStore.swift
+++ apps/ios/LiveVideo/LiveVideo/Twilio/Sync/ViewerStore.swift
@@ -8,22 +8,6 @@ import TwilioSyncClient
 // TODO: Maybe change unique name
 class ViewerStore: NSObject, SyncStoring, ObservableObject {
     let speakerInvitePublisher = PassthroughSubject<Void, Never>()
-    var isHandRaised = false {
-        didSet {
-            document?.mutateData(
-                {
-                    $0?["hand_raised"] = self.isHandRaised // TODO: Test to make sure I don't need weak self
-                    return $0
-                },
-                metadata: nil,
-                completion: { [weak self] result, _ in
-                    if let error = result.error {
-                        self?.handleError(error)
-                    }
-                }
-            )
-        }
-    }
     var documentName: String! // TODO: Maybe rename
     var errorHandler: ((Error) -> Void)?
     private var document: TWSDocument?
@@ -44,7 +28,6 @@ class ViewerStore: NSObject, SyncStoring, ObservableObject {
     
     func disconnect() {
         document = nil
-        isHandRaised = false
     }
     
     private func handleError(_ error: Error) {
