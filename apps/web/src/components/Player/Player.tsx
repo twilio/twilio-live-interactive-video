50ad57292c9ef539923425c650b16ed9e39bc35d
Olivia Pyskoty <opyskoty@twilio.com>
2021-10-26 13:09:48 -0700

    make usePlayerState hook

--- apps/web/src/components/Player/Player.tsx
+++ apps/web/src/components/Player/Player.tsx
@@ -5,6 +5,7 @@ import PlayerMenuBar from './PlayerMenuBar/PlayerMenuBar';
 import usePlayerContext from '../../hooks/usePlayerContext/usePlayerContext';
 import { useAppState } from '../../state';
 import { useEnqueueSnackbar } from '../../hooks/useSnackbar/useSnackbar';
+import { usePlayerState } from '../../hooks/usePlayerState/usePlayerState';
 
 TwilioPlayer.telemetry.subscribe(data => {
   const method = data.name === 'error' ? 'error' : 'log';
@@ -25,39 +26,52 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default function Player() {
+function Player() {
   const classes = useStyles();
   const videoElRef = useRef<HTMLVideoElement>(null!);
   const { player, disconnect } = usePlayerContext();
-  const { appState } = useAppState();
+  const state = usePlayerState();
+  const { appState, appDispatch } = useAppState();
   const enqueueSnackbar = useEnqueueSnackbar();
   const [welcomeMessageDisplayed, setWelcomeMessageDisplayed] = useState(false);
 
   useEffect(() => {
     if (player) {
-      if (player?.state === 'idle') {
+      // if (player?.state === 'idle') {
+      //   player.attach(videoElRef.current);
+      //   player.play();
+      // }
+      console.log('state1', state);
+      if (state === 'ready') {
+        console.log('state2', state);
+        appDispatch({ type: 'set-is-loading', isLoading: false });
         player.attach(videoElRef.current);
         player.play();
       }
-
-      if (!welcomeMessageDisplayed) {
-        setWelcomeMessageDisplayed(true);
-        enqueueSnackbar({
-          headline: 'Welcome!',
-          message:
-            "You're now in the audience - you'll be unable to share audio or video. Raise your hand at any time to request to speak.",
-          variant: 'info',
-        });
-      }
     }
-  }, [player, enqueueSnackbar, welcomeMessageDisplayed]);
+  }, [player, appDispatch, state]);
+
+  useEffect(() => {
+    console.log('meage', welcomeMessageDisplayed);
+    if (!welcomeMessageDisplayed) {
+      setWelcomeMessageDisplayed(true);
+      enqueueSnackbar({
+        headline: 'Welcome!',
+        message:
+          "You're now in the audience - you'll be unable to share audio or video. Raise your hand at any time to request to speak.",
+        variant: 'info',
+      });
+    }
+  }, [enqueueSnackbar, welcomeMessageDisplayed]);
 
   return (
     <div style={{ height: '100vh' }}>
       <div className={classes.container}>
-        <video className={classes.video} ref={videoElRef} playsInline></video>
+        <video className={classes.video} ref={videoElRef} playsInline autoPlay></video>
       </div>
       <PlayerMenuBar roomName={appState.eventName} disconnect={disconnect} />
     </div>
   );
 }
+
+export default React.memo(Player);
--- apps/web/src/components/PlayerProvider/index.tsx
+++ apps/web/src/components/PlayerProvider/index.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useCallback, useState } from 'react';
 import { Player as TwilioPlayer } from '@twilio/player-sdk';
+import { useAppState } from '../../state';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
 
 TwilioPlayer.setLogLevel(TwilioPlayer.LogLevel.Error);
@@ -20,18 +21,21 @@ export const PlayerProvider: React.FC = ({ children }) => {
     (token: string) => {
       const { protocol, host } = window.location;
 
-      return TwilioPlayer.connect(token, {
-        playerWasmAssetsPath: `${protocol}//${host}/player`,
-      })
-        .then(newPlayer => {
-          setPlayer(newPlayer);
-          // @ts-ignore
-          window.twilioPlayer = newPlayer;
+      return (
+        TwilioPlayer.connect(token, {
+          playerWasmAssetsPath: `${protocol}//${host}/player`,
         })
-        .catch(e => {
-          console.log(e);
-          onError(new Error('There was a problem connecting to the Twilio Live Stream.'));
-        });
+          //need to listen state change event and when it is "Ready", then setPlayer and expose to window...
+          .then(newPlayer => {
+            setPlayer(newPlayer);
+            // @ts-ignore
+            window.twilioPlayer = newPlayer;
+          })
+          .catch(e => {
+            console.log(e);
+            onError(new Error('There was a problem connecting to the Twilio Live Stream.'));
+          })
+      );
     },
     [onError]
   );
