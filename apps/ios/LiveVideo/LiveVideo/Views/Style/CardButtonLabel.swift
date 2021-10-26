a8c25296d88a9e729223d04c271f8d094e812eea
Tim Rozum <trozum@twilio.com>
2021-10-26 13:06:32 -0600

    Clean up card button

--- apps/ios/LiveVideo/LiveVideo/Views/Style/CardButtonLabel.swift
+++ apps/ios/LiveVideo/LiveVideo/Views/Style/CardButtonLabel.swift
@@ -4,60 +4,6 @@
 
 import SwiftUI
 
-//struct CardButtonLabel: View {
-//    let title: String
-//    let image: Image
-//    let imageColor: Color
-//    var detail: String?
-//
-//    var body: some View {
-//        ZStack {
-//            RoundedRectangle(cornerRadius: 4)
-//                .foregroundColor(.white)
-//                .shadow(color: .shadowLow, radius: 8, x: 0, y: 2)
-//            RoundedRectangle(cornerRadius: 4)
-//                .stroke(Color.borderWeaker, lineWidth: 1)
-//
-//            VStack {
-//                HStack {
-//                    ZStack {
-//                        imageColor
-//                            .opacity(0.08)
-//                            .clipShape(Circle())
-//                        image
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .padding(10)
-//                            .foregroundColor(imageColor)
-//                    }
-//                    .frame(width: 40, height: 40)
-//
-//                    Text(title)
-//                        .foregroundColor(.textWeak)
-//                        .fontWeight(.bold)
-//                        .lineLimit(1)
-//
-//                    Spacer()
-//                    Image(systemName: "arrow.right")
-//                        .foregroundColor(.backgroundPrimary)
-//                        .font(.system(size: 20, weight: .medium))
-//                        .padding(.trailing, 8)
-//                }
-//                .padding(16)
-//
-//                if let detail = detail {
-//                    Text(detail)
-//                        .multilineTextAlignment(.leading)
-//                }
-//
-//            }
-//
-//
-//        }
-//        .fixedSize(horizontal: false, vertical: true)
-//    }
-//}
-
 struct CardButtonLabel: View {
     let title: String
     let image: Image
@@ -72,7 +18,7 @@ struct CardButtonLabel: View {
             RoundedRectangle(cornerRadius: 4)
                 .stroke(Color.borderWeaker, lineWidth: 1)
             
-            HStack(alignment: .imageTitleAlignmentGuide) {
+            HStack(alignment: .titleAndImages) {
                 ZStack {
                     imageColor
                         .opacity(0.08)
@@ -82,18 +28,14 @@ struct CardButtonLabel: View {
                         .aspectRatio(contentMode: .fit)
                         .padding(10)
                         .foregroundColor(imageColor)
-                        .alignmentGuide(.imageTitleAlignmentGuide) { context in
-                            context[VerticalAlignment.center]
-                        }
+                        .alignmentGuide(.titleAndImages) { $0[VerticalAlignment.center] }
                 }
                 .frame(width: 40, height: 40)
 
                 VStack(alignment: .leading, spacing: 8) {
                     Text(title)
                         .fontWeight(.bold)
-                        .alignmentGuide(.imageTitleAlignmentGuide) { context in
-                            context[VerticalAlignment.center]
-                        }
+                        .alignmentGuide(.titleAndImages) { $0[VerticalAlignment.center] }
                         .multilineTextAlignment(.leading)
 
                     if let detail = detail {
@@ -109,80 +51,61 @@ struct CardButtonLabel: View {
                     .foregroundColor(.backgroundPrimary)
                     .font(.system(size: 20, weight: .medium))
                     .padding(.trailing, 8)
-                    .alignmentGuide(.imageTitleAlignmentGuide) { context in
-                        context[VerticalAlignment.center]
-                    }
+                    .alignmentGuide(.titleAndImages) { $0[VerticalAlignment.center] }
             }
             .padding(16)
-
         }
         .fixedSize(horizontal: false, vertical: true)
     }
 }
 
 // https://developer.apple.com/documentation/swiftui/aligning-views-across-stacks
-extension VerticalAlignment {
-    /// A custom alignment for image titles.
-    private struct ImageTitleAlignment: AlignmentID {
+private extension VerticalAlignment {
+    private struct TitleAndImages: AlignmentID {
         static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            // Default to bottom alignment if no guides are set.
-            context[VerticalAlignment.bottom]
+            context[VerticalAlignment.center]
         }
     }
 
-    /// A guide for aligning titles.
-    static let imageTitleAlignmentGuide = VerticalAlignment(
-        ImageTitleAlignment.self
-    )
+    static let titleAndImages = VerticalAlignment(TitleAndImages.self)
 }
 
 struct CardButton_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            Button(action: { }) {
-                CardButtonLabel(
-                    title: "Title",
-                    image: Image(systemName: "mic")
-                )
-            }
-            .previewDisplayName("Default Image Color")
+            CardButtonLabel(
+                title: "Title",
+                image: Image(systemName: "mic")
+            )
+                .previewDisplayName("Default Image Color")
             
-            Button(action: { }) {
-                CardButtonLabel(
-                    title: "Title",
-                    image: Image(systemName: "plus.square"),
-                    imageColor: .backgroundSuccess
-                )
-            }
-            .previewDisplayName("Custom Image Color")
-
-            Button(action: { }) {
-                CardButtonLabel(
-                    title: String(repeating: "Title ", count: 10),
-                    image: Image(systemName: "mic")
-                )
-            }
-            .previewDisplayName("Long Title")
-
-            Button(action: { }) {
-                CardButtonLabel(
-                    title: "Title",
-                    image: Image(systemName: "mic"),
-                    detail: String(repeating: "Detail ", count: 20)
-                )
-            }
-            .previewDisplayName("Detail")
-
-            Button(action: { }) {
-                CardButtonLabel(
-                    title: String(repeating: "Title ", count: 10),
-                    image: Image(systemName: "mic"),
-                    detail: String(repeating: "Detail ", count: 20)
-                )
-            }
-            .previewDisplayName("Long Title with Detail")
+            CardButtonLabel(
+                title: "Title",
+                image: Image(systemName: "plus.square"),
+                imageColor: .backgroundSuccess
+            )
+                .previewDisplayName("Custom Image Color")
+            
+            CardButtonLabel(
+                title: String(repeating: "Title ", count: 10),
+                image: Image(systemName: "mic")
+            )
+                .previewDisplayName("Long Title")
+            
+            CardButtonLabel(
+                title: "Title",
+                image: Image(systemName: "mic"),
+                detail: String(repeating: "Detail ", count: 20)
+            )
+                .previewDisplayName("Detail")
+            
+            CardButtonLabel(
+                title: String(repeating: "Title ", count: 10),
+                image: Image(systemName: "mic"),
+                detail: String(repeating: "Detail ", count: 20)
+            )
+                .previewDisplayName("Long Title with Detail")
         }
-        
         .previewLayout(.sizeThatFits)
         .padding()
     }
