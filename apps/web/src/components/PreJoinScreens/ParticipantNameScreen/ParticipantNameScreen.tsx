4ce2fe08499aace1706abb937550ac5ef39e76f8
Lee <leewhite128@gmail.com>
2021-12-06 12:56:18 -0800

    add paste, update participant name screen

--- apps/web/src/components/PreJoinScreens/ParticipantNameScreen/ParticipantNameScreen.tsx
+++ apps/web/src/components/PreJoinScreens/ParticipantNameScreen/ParticipantNameScreen.tsx
@@ -1,51 +1,18 @@
 import React, { ChangeEvent, FormEvent } from 'react';
 import { appActionTypes, ActiveScreen, appStateTypes } from '../../../state/appState/appReducer';
-import { Typography, makeStyles, TextField, Grid, Button, InputLabel, Theme } from '@material-ui/core';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  gutterBottom: {
-    fontWeight: 'bold',
-    marginBottom: '1em',
-  },
-  welcome: {
-    color: '#606B85',
-    marginBottom: '0.3em',
-    fontWeight: 'bold',
-  },
-  askName: {
-    fontWeight: 'bold',
-    marginBottom: '2.3em',
-    color: '#606B85',
-  },
-  inputContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: '1.5em 0 3.5em',
-    '& div:not(:last-child)': {
-      marginRight: '1em',
-    },
-    [theme.breakpoints.down('sm')]: {
-      margin: '1.5em 0 2em',
-    },
-  },
-  textFieldContainer: {
-    width: '100%',
-  },
-  continueButton: {
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-    },
-  },
-}));
-
+import { Heading } from '@twilio-paste/core/heading';
+import { Text } from '@twilio-paste/core/text';
+import { Input } from '@twilio-paste/core/input';
+import { Label } from '@twilio-paste/core/label';
+import { Button } from '@twilio-paste/core/button';
+import { Box } from '@twilio-paste/core/box';
 interface ParticipantNameScreenProps {
   state: appStateTypes;
   dispatch: React.Dispatch<appActionTypes>;
 }
 
 export default function ParticipantNameScreen({ state, dispatch }: ParticipantNameScreenProps) {
-  const classes = useStyles();
-
   const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
     dispatch({ type: 'set-participant-name', participantName: event.target.value });
   };
@@ -56,47 +23,24 @@ export default function ParticipantNameScreen({ state, dispatch }: ParticipantNa
   };
 
   return (
-    <div>
-      <Typography variant="h5" className={classes.welcome}>
-        Welcome to:
-      </Typography>
-      <Typography variant="h4" className={classes.gutterBottom}>
+    <Box>
+      <Text as="p" fontWeight="fontWeightBold" color="colorTextWeak" marginBottom="space20" fontSize="fontSize70">
+        Welcome to
+      </Text>
+      <Heading as="h1" variant="heading10">
         Twilio Live Video Events
-      </Typography>
-      <Typography variant="body1" className={classes.askName}>
-        What's your name?
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <div className={classes.inputContainer}>
-          <div className={classes.textFieldContainer}>
-            <InputLabel
-              htmlFor="input-user-name"
-              style={{ fontWeight: 'bold', fontSize: '0.8rem', marginBottom: '0.5em' }}
-            >
-              Full Name
-            </InputLabel>
-            <TextField
-              id="input-user-name"
-              variant="outlined"
-              fullWidth
-              size="small"
-              value={state.participantName}
-              onChange={handleNameChange}
-            />
-          </div>
-        </div>
-        <Grid container justifyContent="flex-end">
-          <Button
-            variant="contained"
-            type="submit"
-            color="primary"
-            disabled={!state.participantName}
-            className={classes.continueButton}
-          >
+      </Heading>
+      <Box as="form" onSubmit={handleSubmit}>
+        <Box width="100%" marginTop="space70" marginBottom="space150">
+          <Label htmlFor="input-user-name">Full Name</Label>
+          <Input type="text" id="input-user-name" value={state.participantName} onChange={handleNameChange} />
+        </Box>
+        <Box display="flex" justifyContent="flex-end">
+          <Button variant="primary" type="submit" disabled={!state.participantName}>
             Continue
           </Button>
-        </Grid>
-      </form>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   );
 }
