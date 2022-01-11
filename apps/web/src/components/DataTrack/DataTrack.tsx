761d7929eab4b2b1bf1f2ae9a28f7afa600cd813
Tim Mendoza <tmendoza@twilio.com>
2022-01-10 22:45:04 -0700

    Dispatch snackbar when a host mutes a speakers audio

--- apps/web/src/components/DataTrack/DataTrack.tsx
+++ apps/web/src/components/DataTrack/DataTrack.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import { DataTrack as IDataTrack } from 'twilio-video';
+import { useEnqueueSnackbar } from '../../hooks/useSnackbar/useSnackbar';
 import useLocalAudioToggle from '../../hooks/useLocalAudioToggle/useLocalAudioToggle';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
 
 export default function DataTrack({ track }: { track: IDataTrack }) {
   const { room } = useVideoContext();
   const [isAudioEnabled, toggleAudio] = useLocalAudioToggle();
+  const enqueueSnackbar = useEnqueueSnackbar();
 
   useEffect(() => {
     const handleMessage = (message: string) => {
@@ -17,6 +19,11 @@ export default function DataTrack({ track }: { track: IDataTrack }) {
         ) {
           if (isAudioEnabled) {
             toggleAudio();
+            enqueueSnackbar({
+              headline: '',
+              message: 'Your microphone has been muted by the host.',
+              variant: 'warning',
+            });
           }
         }
       } catch (e) {
