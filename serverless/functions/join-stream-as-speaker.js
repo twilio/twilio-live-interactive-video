059aa90b2ff87f3213dca915119b5d3bf32967d5
Tim Mendoza <tmendoza@twilio.com>
2022-03-22 21:25:42 -0600

    Refactor errors in join-stream; add permissions to stream document

--- serverless/functions/join-stream-as-speaker.js
+++ serverless/functions/join-stream-as-speaker.js
@@ -8,8 +8,7 @@ const SyncGrant = AccessToken.SyncGrant;
 const MAX_ALLOWED_SESSION_DURATION = 14400;
 
 module.exports.handler = async (context, event, callback) => {
-  const { ACCOUNT_SID, TWILIO_API_KEY_SID, TWILIO_API_KEY_SECRET, CONVERSATIONS_SERVICE_SID } =
-    context;
+  const { ACCOUNT_SID, TWILIO_API_KEY_SID, TWILIO_API_KEY_SECRET, CONVERSATIONS_SERVICE_SID } = context;
 
   const authHandler = require(Runtime.getAssets()['/auth.js'].path);
   authHandler(context, event, callback);
@@ -17,7 +16,7 @@ module.exports.handler = async (context, event, callback) => {
   const { user_identity, stream_name } = event;
 
   const common = require(Runtime.getAssets()['/common.js'].path);
-  const { getStreamMapItem } = common(context, event, callback);
+  const { getStreamMapItem, createErrorHandler } = common(context, event, callback);
 
   let response = new Twilio.Response();
   response.appendHeader('Content-Type', 'application/json');
@@ -48,156 +47,80 @@ module.exports.handler = async (context, event, callback) => {
 
   const client = context.getTwilioClient();
 
-  try {
-    // See if a room already exists
-    room = await client.video.rooms(stream_name).fetch();
-  } catch (e) {
-    response.setStatusCode(500);
-    response.setBody({
-      error: {
-        message: 'error finding room',
-        explanation: e.message,
-      },
-    });
-    return callback(null, response);
-  }
+  // See if a room already exists
+  room = await client.video.rooms(stream_name).fetch().catch(createErrorHandler('error finding room'));
 
   // Fetch stream map item
-  try {
-    streamMapItem = await getStreamMapItem(room.sid);
-  } catch (e) {
-    response.setStatusCode(500);
-    response.setBody({
-      error: {
-        message: 'error finding stream map item',
-        explanation: e.message,
-      },
-    });
-    return callback(null, response);
-  }
+  streamMapItem = await getStreamMapItem(room.sid).catch(createErrorHandler('error finding stream map item'));
 
   const streamSyncClient = client.sync.services(streamMapItem.data.sync_service_sid);
 
   const userDocumentName = `user-${user_identity}`;
   // Create user document
-  try {
-    await streamSyncClient.documents.create({
+  await streamSyncClient.documents
+    .create({
       uniqueName: userDocumentName,
+    })
+    .catch((error) => {
+      // Ignore "Unique name already exists" error
+      if (error.code !== 54301) {
+        createErrorHandler('error creating user document')(error);
+      }
     });
-  } catch (e) {
-    // Ignore "Unique name already exists" error
-    if (e.code !== 54301) {
-      console.error(e);
-      response.setStatusCode(500);
-      response.setBody({
-        error: {
-          message: 'error creating user document',
-          explanation: e.message,
-        },
-      });
-      return callback(null, response);
-    }
-  }
+
+  // Give user read access to stream document
+  await streamSyncClient
+    .documents('stream')
+    .documentPermissions(user_identity)
+    .update({ read: true, write: false, manage: false })
+    .catch(createErrorHandler('error adding read access to stream document.'));
 
   // Give user read access to user document
-  try {
-    await streamSyncClient.documents(userDocumentName)
-      .documentPermissions(user_identity)
-      .update({ read: true, write: false, manage: false })
-  } catch (e) {
-    response.setStatusCode(500);
-    response.setBody({
-      error: {
-        message: 'error adding read access to user document',
-        explanation: e.message,
-      },
-    });
-    return callback(null, response);
-  }
-  
+  await streamSyncClient
+    .documents(userDocumentName)
+    .documentPermissions(user_identity)
+    .update({ read: true, write: false, manage: false })
+    .catch(createErrorHandler('error adding read access to user document'));
+
   // Give user read access to speakers map
-  try {
-    await streamSyncClient.syncMaps('speakers')
-      .syncMapPermissions(user_identity)
-      .update({ read: true, write: false, manage: false })
-  } catch (e) {
-    response.setStatusCode(500);
-    response.setBody({
-      error: {
-        message: 'error adding read access to speakers map',
-        explanation: e.message,
-      },
-    });
-    return callback(null, response);
-  }
-  
-  const raisedHandsMapName = `raised_hands`;
+  await streamSyncClient
+    .syncMaps('speakers')
+    .syncMapPermissions(user_identity)
+    .update({ read: true, write: false, manage: false })
+    .catch(createErrorHandler('error adding read access to speakers map'));
+
   // Give user read access to raised hands map
-  try {
-    await streamSyncClient.syncMaps(raisedHandsMapName)
-      .syncMapPermissions(user_identity)
-      .update({ read: true, write: false, manage: false });
-  } catch (e) {
-    response.setStatusCode(500);
-    response.setBody({
-      error: {
-        message: 'error adding read access to raised hands map',
-        explanation: e.message,
-      },
-    });
-    return callback(null, response);
-  }
+  await streamSyncClient
+    .syncMaps('raised_hands')
+    .syncMapPermissions(user_identity)
+    .update({ read: true, write: false, manage: false })
+    .catch(createErrorHandler('error adding read access to raised hands map'));
 
   // Give user read access to viewers map
-  try {
-    await streamSyncClient.syncMaps('viewers')
-      .syncMapPermissions(user_identity)
-      .update({ read: true, write: false, manage: false })
-  } catch (e) {
-    response.setStatusCode(500);
-    response.setBody({
-      error: {
-        message: 'error adding read access to viewers map',
-        explanation: e.message,
-      },
-    });
-    return callback(null, response);
-  }
+  await streamSyncClient
+    .syncMaps('viewers')
+    .syncMapPermissions(user_identity)
+    .update({ read: true, write: false, manage: false })
+    .catch(createErrorHandler('error adding read access to viewers map'));
 
   const conversationsClient = client.conversations.services(CONVERSATIONS_SERVICE_SID);
 
-  try {
-    // Find conversation
-    conversation = await conversationsClient.conversations(room.sid).fetch();
-  } catch (e) {
-    console.error(e);
-    response.setStatusCode(500);
-    response.setBody({
-      error: {
-        message: 'error finding conversation',
-        explanation: 'Something went wrong when finding a conversation.',
-      },
-    });
-    return callback(null, response);
-  }
+  // Find conversation
+  conversation = await conversationsClient
+    .conversations(room.sid)
+    .fetch()
+    .catch(createErrorHandler('error finding conversation'));
 
-  try {
-    // Add participant to conversation
-    await conversationsClient.conversations(room.sid).participants.create({ identity: user_identity });
-  } catch (e) {
-    // Ignore "Participant already exists" error (50433)
-    if (e.code !== 50433) {
-      console.error(e);
-      response.setStatusCode(500);
-      response.setBody({
-        error: {
-          message: 'error creating conversation participant',
-          explanation: 'Something went wrong when creating a conversation participant.',
-        },
-      });
-      return callback(null, response);
-    }
-  }
+  // Add participant to conversation
+  await conversationsClient
+    .conversations(room.sid)
+    .participants.create({ identity: user_identity })
+    .catch((error) => {
+      // Ignore "Participant already exists" error (50433)
+      if (error.code !== 50433) {
+        createErrorHandler('error adding read access to user document')(error);
+      }
+    });
 
   // Create token
   const token = new AccessToken(ACCOUNT_SID, TWILIO_API_KEY_SID, TWILIO_API_KEY_SECRET, {
@@ -226,8 +149,9 @@ module.exports.handler = async (context, event, callback) => {
     sync_object_names: {
       speakers_map: 'speakers',
       viewers_map: 'viewers',
-      raised_hands_map: `raised_hands`,
-      user_document: `user-${user_identity}`,
+      raised_hands_map: 'raised_hands',
+      user_document: userDocumentName,
+      stream_document: 'stream',
     },
   });
   return callback(null, response);
