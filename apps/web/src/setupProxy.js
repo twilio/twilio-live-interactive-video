30f21906d5da323cf1dea7b90659073179a29eb6
Tim Mendoza <tmendoza@twilio.com>
2022-03-30 14:47:16 -0600

    Fix proxy

--- apps/web/src/setupProxy.js
+++ apps/web/src/setupProxy.js
@@ -4,8 +4,8 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../../../.env') });
 
 module.exports = function(app) {
-  app.use(
-    '/',
+  app.post(
+    '*',
     createProxyMiddleware({
       target: process.env.WEB_PROXY_URL,
       changeOrigin: true,
