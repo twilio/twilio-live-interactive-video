b2a490e6d2e0ff40972ebc78cc7ca25cfc46476e
Tim Mendoza <tmendoza@twilio.com>
2022-04-11 20:10:55 -0600

    Add recording error to useIsRecording hook

--- apps/web/src/hooks/useIsRecording/useIsRecording.ts
+++ apps/web/src/hooks/useIsRecording/useIsRecording.ts
@@ -6,12 +6,18 @@ import useVideoContext from '../useVideoContext/useVideoContext';
 export default function useIsRecording() {
   const { room } = useVideoContext();
   const [isRecording, setIsRecording] = useState(Boolean(room?.isRecording));
+  const [recordingError, setRecordingError] = useState<null | string>(null);
   const { streamDocument } = useSyncContext();
 
   useEffect(() => {
     if (streamDocument) {
       setIsRecording((streamDocument?.data as StreamDocument).recording.is_recording);
-      const handleUpdate = (update: { data: StreamDocument }) => setIsRecording(update.data.recording.is_recording);
+      setRecordingError((streamDocument?.data as StreamDocument).recording.error);
+
+      const handleUpdate = (update: { data: StreamDocument }) => {
+        setIsRecording(update.data.recording.is_recording);
+        setRecordingError(update.data.recording.error);
+      };
 
       streamDocument.on('updated', handleUpdate);
       return () => {
@@ -20,5 +26,5 @@ export default function useIsRecording() {
     }
   }, [streamDocument]);
 
-  return isRecording;
+  return { isRecording, recordingError };
 }
