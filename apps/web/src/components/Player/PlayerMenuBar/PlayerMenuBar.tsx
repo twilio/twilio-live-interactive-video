8ac0b5b454ce081627a8a89c663d4bfb137ca92c
Olivia Pyskoty <opyskoty@twilio.com>
2022-06-29 16:06:39 -0700

    update PlayerMenuBar buttons to be consistent with iOS

--- apps/web/src/components/Player/PlayerMenuBar/PlayerMenuBar.tsx
+++ apps/web/src/components/Player/PlayerMenuBar/PlayerMenuBar.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useRef, useState } from 'react';
+import clsx from 'clsx';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Typography, Grid, Button, useMediaQuery, Hidden } from '@material-ui/core';
+import { Typography, Grid, Button, Hidden } from '@material-ui/core';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { useAppState } from '../../../state';
 import { raiseHand } from '../../../state/api/api';
 import { useEnqueueSnackbar } from '../../../hooks/useSnackbar/useSnackbar';
@@ -26,7 +28,28 @@ const useStyles = makeStyles((theme: Theme) =>
       [theme.breakpoints.down('sm')]: {
         height: `${theme.mobileFooterHeight}px`,
         padding: 0,
+        '& .MuiButton-startIcon': {
+          margin: 0,
+        },
       },
+    },
+    mobileBackground: {
+      [theme.breakpoints.down('sm')]: {
+        marginRight: '1em',
+        padding: '12px',
+        borderRadius: '50%',
+        minWidth: 0,
+        height: '40px',
+        width: '40px',
+        '&:not(:last-child)': {
+          background: '#E1E3EA',
+        },
+      },
+    },
+    mobileRoomLabel: {
+      display: 'flex',
+      alignItems: 'center',
+      marginLeft: '0.5em',
     },
     button: {
       background: theme.brand,
@@ -40,7 +63,6 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function PlayerMenuBar({ roomName, disconnect }: { roomName?: string; disconnect: () => void }) {
   const classes = useStyles();
-  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
   const { appState, appDispatch } = useAppState();
   const { setIsChatWindowOpen, isChatWindowOpen } = useChatContext();
   const [isHandRaised, setIsHandRaised] = useState(false);
@@ -90,27 +112,42 @@ export default function PlayerMenuBar({ roomName, disconnect }: { roomName?: str
 
   return (
     <footer className={classes.container}>
-      <Grid container justifyContent="space-around" alignItems="center">
-        <Grid style={{ flex: isMobile ? 0 : 1 }}>
-          <Typography variant="body1">{roomName}</Typography>
-        </Grid>
+      <Hidden mdUp>
+        <Typography variant="body1" className={classes.mobileRoomLabel}>
+          {roomName}
+        </Typography>
+      </Hidden>
+      <Grid container justifyContent="center" alignItems="center">
+        <Hidden smDown>
+          <Grid style={{ flex: 1 }}>
+            <Typography variant="body1">{roomName}</Typography>
+          </Grid>
+        </Hidden>
         <Grid item>
-          <Grid container justifyContent="center">
-            <Button onClick={handleRaiseHand} startIcon={isHandRaised ? <LowerHandIcon /> : <RaiseHandIcon />}>
+          <Grid container>
+            <Button
+              onClick={handleRaiseHand}
+              className={classes.mobileBackground}
+              startIcon={isHandRaised ? <LowerHandIcon /> : <RaiseHandIcon />}
+            >
               <Hidden smDown>{isHandRaised ? 'Lower Hand' : 'Raise Hand'}</Hidden>
             </Button>
-            <Button onClick={() => toggleParticipantWindow()} startIcon={<ParticipantIcon />}>
+            <Button
+              onClick={() => toggleParticipantWindow()}
+              className={classes.mobileBackground}
+              startIcon={<ParticipantIcon />}
+            >
               <Hidden smDown>Participants</Hidden>
             </Button>
             {appState.isChatEnabled && (
-              <Button onClick={() => toggleChatWindow()} startIcon={<ChatIcon />}>
+              <Button onClick={() => toggleChatWindow()} className={classes.mobileBackground} startIcon={<ChatIcon />}>
                 <Hidden smDown>Chat</Hidden>
               </Button>
             )}
 
             <Hidden mdUp>
-              <Button onClick={disconnectFromEvent} className={classes.button}>
-                Leave Stream
+              <Button onClick={disconnectFromEvent} className={clsx(classes.button, classes.mobileBackground)}>
+                <ExitToAppIcon />
               </Button>
             </Hidden>
           </Grid>
