de75c48b21e4a63e0e7857df0a58ebe93db51420
Edwin Cobos <ecobos@twilio.com>
2022-07-28 21:25:39 -0500

    Refactor for RemoteStorage adn LiveVideoRequestInterceptor in order to parse the error body from the interceptor.

--- apps/android/LiveVideoApp/app/src/main/java/com/twilio/livevideo/app/repository/datasource/remote/RemoteStorage.kt
+++ apps/android/LiveVideoApp/app/src/main/java/com/twilio/livevideo/app/repository/datasource/remote/RemoteStorage.kt
@@ -1,54 +1,42 @@
 package com.twilio.livevideo.app.repository.datasource.remote
 
-import com.google.gson.Gson
 import com.twilio.livevideo.app.repository.model.BaseResponse
 import com.twilio.livevideo.app.repository.model.CreateStreamResponse
 import com.twilio.livevideo.app.repository.model.DeleteStreamResponse
 import com.twilio.livevideo.app.repository.model.JoinStreamAsViewerResponse
 import com.twilio.livevideo.app.repository.model.VerifyPasscodeResponse
-import com.twilio.livevideo.app.util.ApiResponseUtil
 import retrofit2.Response
 import javax.inject.Inject
 
 class RemoteStorage @Inject constructor(private var liveVideoAPIService: LiveVideoAPIService) {
 
-    private val gson = Gson()
-
-    suspend fun verifyPasscode(passcode: String): VerifyPasscodeResponse =
-        processResponse(liveVideoAPIService.verifyPasscode(passcode))
+    suspend fun verifyPasscode(passcode: String): VerifyPasscodeResponse = liveVideoAPIService.verifyPasscode(passcode).let { response ->
+        (response.body() ?: VerifyPasscodeResponse()).also { setBaseResponse(it, response) }
+    }
 
     suspend fun joinStreamAsViewer(
         userIdentity: String,
         streamName: String
-    ): JoinStreamAsViewerResponse =
-        processResponse(liveVideoAPIService.joinStreamAsViewer(userIdentity, streamName))
+    ): JoinStreamAsViewerResponse = liveVideoAPIService.joinStreamAsViewer(userIdentity, streamName).let { response ->
+        (response.body() ?: JoinStreamAsViewerResponse()).also { setBaseResponse(it, response) }
+    }
 
     suspend fun createStream(
         userIdentity: String,
         streamName: String
-    ): CreateStreamResponse =
-        processResponse(liveVideoAPIService.createStream(userIdentity, streamName))
+    ): CreateStreamResponse = liveVideoAPIService.createStream(userIdentity, streamName).let { response ->
+        (response.body() ?: CreateStreamResponse()).also { setBaseResponse(it, response) }
+    }
 
     suspend fun deleteStream(
         streamName: String
-    ): DeleteStreamResponse =
-        processResponse(liveVideoAPIService.deleteStream(streamName))
+    ): DeleteStreamResponse = liveVideoAPIService.deleteStream(streamName).let { response ->
+        (response.body() ?: DeleteStreamResponse()).also { setBaseResponse(it, response) }
+    }
 
-    private inline fun <reified T : BaseResponse> processResponse(
-        response: Response<T>,
-    ): T {
-        val result: T = response.body() ?: T::class.java.newInstance()
+    private fun <T : BaseResponse> setBaseResponse(result: T, response: Response<T>) {
         result.code = response.code()
-        result.isApiResponseSuccess = response.errorBody() == null
-        if (!response.isSuccessful) { // Failure case
-            ApiResponseUtil.parseErrorBody(
-                gson,
-                response.errorBody(),
-                result,
-                T::class.java
-            )
-        }
-        return result
+        result.isApiResponseSuccess = response.code() != 299
     }
 
 }
