ad0b6e8faa6d2cfdda4bdb95955ed96b02c2de5c
Olivia Pyskoty <opyskoty@twilio.com>
2022-08-03 11:48:14 -0700

    remove firebase user info from UserAvatar

--- apps/web/src/components/IntroContainer/UserMenu/UserAvatar/UserAvatar.tsx
+++ apps/web/src/components/IntroContainer/UserMenu/UserAvatar/UserAvatar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import makeStyles from '@material-ui/styles/makeStyles';
 import Person from '@material-ui/icons/Person';
-import { StateContextType } from '../../../../state';
 
 const useStyles = makeStyles({
   red: {
@@ -11,21 +10,12 @@ const useStyles = makeStyles({
   },
 });
 
-export function getInitials(name: string) {
-  return name
-    .split(' ')
-    .map(text => text[0])
-    .join('')
-    .toUpperCase();
-}
-
-export default function UserAvatar({ user }: { user: StateContextType['user'] }) {
+export default function UserAvatar() {
   const classes = useStyles();
-  const { displayName, photoURL } = user!;
 
-  return photoURL ? (
-    <Avatar src={photoURL} />
-  ) : (
-    <Avatar className={classes.red}>{displayName ? getInitials(displayName) : <Person />}</Avatar>
+  return (
+    <Avatar className={classes.red}>
+      <Person />
+    </Avatar>
   );
 }
