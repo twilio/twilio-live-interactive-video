2f2aad642cd92d4ce4c37e31abe21275342793ff
Edwin Cobos <ecobos@twilio.com>
2022-08-03 15:44:21 -0500

    - Fix for Test cases after remoteStorage refactor

--- apps/android/LiveVideoApp/app/src/test/java/com/twilio/livevideo/app/di/MainModuleFake.kt
+++ apps/android/LiveVideoApp/app/src/test/java/com/twilio/livevideo/app/di/MainModuleFake.kt
@@ -1,5 +1,8 @@
 package com.twilio.livevideo.app.di
 
+import com.twilio.livevideo.app.BuildConfig
+import com.twilio.livevideo.app.manager.AuthenticatorManager
+import com.twilio.livevideo.app.network.LiveVideoRequestInterceptor
 import com.twilio.livevideo.app.repository.datasource.local.LocalStorage
 import com.twilio.livevideo.app.repository.datasource.local.LocalStorageImplFake
 import com.twilio.livevideo.app.repository.datasource.remote.LiveVideoAPIService
@@ -9,7 +12,11 @@ import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.mockito.kotlin.mock
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -22,6 +29,29 @@ class MainModuleFake {
     @Singleton
     @Provides
     fun provideTwilioLiveService(): LiveVideoAPIService = mock()
+
+    @Provides
+    fun provideLiveAppInterceptor(authenticatorManager: AuthenticatorManager): LiveVideoRequestInterceptor =
+        LiveVideoRequestInterceptor(authenticatorManager)
+
+    @Provides
+    fun provideOkHttpClient(liveVideoRequestInterceptor: LiveVideoRequestInterceptor): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(liveVideoRequestInterceptor)
+            .cache(null)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitClient(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
+        .build()
 
     @Provides
     @Singleton
