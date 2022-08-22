bf34b58253a93de9010841949f329333cfb98679
Edwin Cobos <ecobos@twilio.com>
2022-08-22 11:56:07 -0500

    Task/video 10785/improvement remote storage to parse error using retrofit (#156)
    * - Refactor for RemoteStorage process to validate and parsing Error responses using the retrofit parsing process.
    
    * - Fix for Test cases after remoteStorage refactor
    
    * Fix to log and handle the exception MalformedJsonException

--- apps/android/LiveVideoApp/app/src/main/java/com/twilio/livevideo/app/repository/datasource/remote/RemoteStorage.kt
+++ apps/android/LiveVideoApp/app/src/main/java/com/twilio/livevideo/app/repository/datasource/remote/RemoteStorage.kt
@@ -1,60 +1,72 @@
 package com.twilio.livevideo.app.repository.datasource.remote
 
-import com.google.gson.Gson
+import com.google.gson.stream.MalformedJsonException
 import com.twilio.livevideo.app.repository.model.BaseResponse
 import com.twilio.livevideo.app.repository.model.CreateStreamResponse
 import com.twilio.livevideo.app.repository.model.DeleteStreamResponse
+import com.twilio.livevideo.app.repository.model.ErrorResponse
 import com.twilio.livevideo.app.repository.model.JoinStreamAsSpeakerResponse
 import com.twilio.livevideo.app.repository.model.JoinStreamAsViewerResponse
 import com.twilio.livevideo.app.repository.model.VerifyPasscodeResponse
-import com.twilio.livevideo.app.util.ApiResponseUtil
 import retrofit2.Response
+import retrofit2.Retrofit
+import timber.log.Timber
 import javax.inject.Inject
 
-class RemoteStorage @Inject constructor(private var liveVideoAPIService: LiveVideoAPIService) {
-
-    private val gson = Gson()
+class RemoteStorage @Inject constructor(private val liveVideoAPIService: LiveVideoAPIService, private val retrofit: Retrofit) {
 
     suspend fun verifyPasscode(passcode: String): VerifyPasscodeResponse =
-        processResponse(liveVideoAPIService.verifyPasscode(passcode))
+        validateResponse(liveVideoAPIService.verifyPasscode(passcode))
 
     suspend fun joinStreamAsViewer(
         userIdentity: String,
         streamName: String
     ): JoinStreamAsViewerResponse =
-        processResponse(liveVideoAPIService.joinStreamAsViewer(userIdentity, streamName))
+        validateResponse(liveVideoAPIService.joinStreamAsViewer(userIdentity, streamName))
 
     suspend fun createStream(
         userIdentity: String,
         streamName: String
     ): CreateStreamResponse =
-        processResponse(liveVideoAPIService.createStream(userIdentity, streamName))
+        validateResponse(liveVideoAPIService.createStream(userIdentity, streamName))
 
     suspend fun deleteStream(
         streamName: String
     ): DeleteStreamResponse =
-        processResponse(liveVideoAPIService.deleteStream(streamName))
+        validateResponse(liveVideoAPIService.deleteStream(streamName))
 
     suspend fun joinStreamAsSpeaker(
         userIdentity: String,
         streamName: String
     ): JoinStreamAsSpeakerResponse =
-        processResponse(liveVideoAPIService.joinStreamAsSpeaker(userIdentity, streamName))
+        validateResponse(liveVideoAPIService.joinStreamAsSpeaker(userIdentity, streamName))
 
-    private inline fun <reified T : BaseResponse> processResponse(
+    private inline fun <reified T : BaseResponse> validateResponse(
         response: Response<T>,
     ): T {
-        val result: T = response.body() ?: T::class.java.newInstance()
+        val result: T = if (!response.isSuccessful) { // Failure case
+            var parsingException: MalformedJsonException? = null
+            response.errorBody()?.let { body ->
+                try {
+                    retrofit.responseBodyConverter<T>(T::class.java, arrayOf()).convert(body)
+                } catch (e: MalformedJsonException) {
+                    parsingException = e
+                    Timber.e(e)
+                    null
+                }
+            } ?: T::class.java.newInstance().apply {
+                val explanation = if (code == 404)
+                    ""
+                else
+                    parsingException?.message ?: "MalformedJsonException"
+                error = ErrorResponse("Error - ${response.code()}", explanation)
+            }
+        } else {
+            response.body()!!
+        }
+
         result.code = response.code()
         result.isApiResponseSuccess = response.errorBody() == null
-        if (!response.isSuccessful) { // Failure case
-            ApiResponseUtil.parseErrorBody(
-                gson,
-                response.errorBody(),
-                result,
-                T::class.java
-            )
-        }
         return result
     }
 
